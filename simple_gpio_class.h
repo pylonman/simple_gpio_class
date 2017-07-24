fa40b18099697b6e7aa189e5921c81625bbbc09f
Jason Fowler <Pylon.man@gmail.com>
2017-07-24 09:41:36 -0400

    Update simple_gpio_class.h

--- simple_gpio_class.h
+++ simple_gpio_class.h
@@ -2,11 +2,9 @@
 #include <Arduino.h>
 #include <stdint.h>
 
-class gpio
+struct gpio
 {
-public:
     gpio(uint8_t pin_arg, uint8_t mode) : pin(pin_arg) { pinMode(pin, mode); };
-public:
     void write(uint8_t value) const { digitalWrite(pin, value); };
     uint8_t read() const { return(digitalRead(pin)); };
     const uint8_t pin;
