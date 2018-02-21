8672cf63c8a821e77bcd14795167c649ac6acdff
Jason Fowler <Pylon.man@gmail.com>
2018-02-21 12:43:29 -0500

    Extend and Simplify object / constructor usage.
     Added an Input / Output object that replaces and inherits GPIO.  Added operator overloads for Input and Output.  Added namespaces for explicit logic operation; active_high, active_low

--- simple_gpio_class.h
+++ simple_gpio_class.h
@@ -2,10 +2,45 @@
 #include <Arduino.h>
 #include <stdint.h>
 
-struct gpio
+namespace simple_gpio
 {
-    gpio(uint8_t pin_arg, uint8_t mode) : pin(pin_arg) { pinMode(pin, mode); };
-    void write(uint8_t value) const { digitalWrite(pin, value); };
-    int read() const { return(digitalRead(pin)); };
-    const uint8_t pin;
-};
+    class gpio
+    {
+    protected:
+        gpio(const uint8_t pin_a): pin_num(pin_a) {};
+        const uint8_t pin_num;
+    public:
+        uint8_t pin() const { return pin_num; };
+    };
+
+    class Output : public gpio
+    {
+    public:
+        Output(const uint8_t pin): gpio(pin) { pinMode(pin_num, OUTPUT); };
+        void set(const uint8_t value) const { digitalWrite(pin_num, value); };
+        void operator =(const uint8_t value) { set(value); };
+        ~Output() { pinMode(pin_num, INPUT); };
+    };
+
+    class Input : public gpio
+    {
+    public:
+        Input(const uint8_t pin): gpio(pin) { pinMode(pin_num, INPUT); };
+        Input(const uint8_t pin, const uint8_t mode): gpio(pin) { pinMode(pin_num, INPUT_PULLUP); };
+        int get() const { return digitalRead(pin_num); };
+        bool operator ==(const int state) { return digitalRead(pin_num) == state ? true : false; };
+        operator bool() { return digitalRead(pin_num); };
+    };
+
+    namespace active_high
+    {
+        const uint8_t high = HIGH;
+        const uint8_t low = LOW;
+    }
+
+    namespace active_low
+    {
+        const uint8_t high = LOW;
+        const uint8_t low = HIGH;
+    }
+}
