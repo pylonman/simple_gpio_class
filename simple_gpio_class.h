7f3431fe6a3555cbec640cfefcdb37e65d4288c3
Jason Fowler <Pylon.man@gmail.com>
2018-02-21 13:55:58 -0500

    Update simple_gpio_class.h

--- simple_gpio_class.h
+++ simple_gpio_class.h
@@ -7,7 +7,7 @@ namespace simple_gpio
     class gpio
     {
     protected:
-        gpio(const uint8_t pin_a): pin_num(pin_a) {};
+        gpio(const uint8_t pin): pin_num(pin) {};
         const uint8_t pin_num;
     public:
         uint8_t pin() const { return pin_num; };
@@ -16,31 +16,31 @@ namespace simple_gpio
     class Output : public gpio
     {
     public:
-        Output(const uint8_t pin): gpio(pin) { pinMode(pin_num, OUTPUT); };
-        void set(const uint8_t value) const { digitalWrite(pin_num, value); };
-        void operator =(const uint8_t value) { set(value); };
+        Output(uint8_t pin): gpio(pin) { pinMode(pin_num, OUTPUT); };
+        void set(uint8_t value) const { digitalWrite(pin_num, value); };
+        void operator =(uint8_t value) { set(value); };
         ~Output() { pinMode(pin_num, INPUT); };
     };
 
     class Input : public gpio
     {
     public:
-        Input(const uint8_t pin): gpio(pin) { pinMode(pin_num, INPUT); };
-        Input(const uint8_t pin, const uint8_t mode): gpio(pin) { pinMode(pin_num, INPUT_PULLUP); };
+        Input(uint8_t pin): gpio(pin) { pinMode(pin_num, INPUT); };
+        Input(uint8_t pin, uint8_t mode): gpio(pin) { pinMode(pin_num, INPUT_PULLUP); };
         int get() const { return digitalRead(pin_num); };
-        bool operator ==(const int state) { return digitalRead(pin_num) == state ? true : false; };
+        bool operator ==(int state) { return digitalRead(pin_num) == state ? true : false; };
         operator bool() { return digitalRead(pin_num); };
     };
 
     namespace active_high
     {
-        const uint8_t high = HIGH;
-        const uint8_t low = LOW;
+        constexpr uint8_t high = HIGH;
+        constexpr uint8_t low = LOW;
     }
 
     namespace active_low
     {
-        const uint8_t high = LOW;
-        const uint8_t low = HIGH;
+        constexpr uint8_t high = LOW;
+        constexpr uint8_t low = HIGH;
     }
 }
